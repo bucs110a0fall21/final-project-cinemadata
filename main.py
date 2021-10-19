3ed6b42160b3f7c45846b54875390b53e3484d73
Kevin Wu <kevinwu163@gmail.com>
2021-10-19 10:16:04 -0400

    Completed requirements for Milestone II

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-#import your controller
+import pygame
 
 def main():
-    #Create an instance on your controller object
+	pygame.init()
+	team = {"lead": "Kevin Wu", "backend": "Daniel Zheng", "frontend": "Wilson Huang"}
+	print("Software Lead is:", team["lead"])
+	print("Backend is:", team["backend"])
+	print("Frontend is:", team["frontend"])
 main()
