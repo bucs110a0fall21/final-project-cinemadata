ac0effceb339c63f2cbbe60d4233d11d48aa8f7c
Daniel Zheng <dzheng47@binghamton.edu>
2021-10-26 12:29:55 -0400

    test commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 def main():
 	pygame.init()
 	team = {"lead": "Kevin Wu", "backend": "Daniel Zheng", "frontend": "Wilson Huang"}
-	print("The Software Lead is:", team["lead"])
-	print("The Backend is:", team["backend"])
-	print("The Frontend is:", team["frontend"])
+	print("Software Lead is:", team["lead"])
+	print("Backend is:", team["backend"])
+	print("rontend is:", team["frontend"])
 main()
