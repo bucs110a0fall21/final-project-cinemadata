2b2fb573ed2e2257937d4cceca89dbca42e1376a
Wilson Huang <--whuang88@binghamton.edu>
2021-10-26 12:16:01 -0400

    test commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 def main():
 	pygame.init()
 	team = {"lead": "Kevin Wu", "backend": "Daniel Zheng", "frontend": "Wilson Huang"}
-	print("Software Lead is:", team["lead"])
-	print("Backend is:", team["backend"])
-	print("Frontend is:", team["frontend"])
+	print("The Software Lead is:", team["lead"])
+	print("The Backend is:", team["backend"])
+	print("The Frontend is:", team["frontend"])
 main()
