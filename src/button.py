d4e6b2a95c239579385c2a181324307dbe46c713
Daniel Zheng <dzheng47@binghamton.edu>
2021-11-12 12:40:34 -0500

    added in decriptions to models and their methods

--- src/button.py
+++ src/button.py
@@ -1,21 +1,24 @@
 import pygame
 
 class Button:
-    def __init__(self, button_type, x, y, img_file):
+    def __init__(self, x, y, img_file):
+        #initiates the button values
+
         pygame.sprite.Sprite.__init__(self)
 
         self.x = x
         self.y = y
-        self.type = button_type
         self.image = pygame.image.load(img_file).convert_alpha()
         self.status = False
         self.genre = ''
-        self.selected_genres = []
 
     def genreButton(self, genre):
+        #Add a genre to the button if it is a genre
         self.genre = genre
+        #Genre is not included as a parameter in the init so that buttons that are not genre buttons dont need to have a genre parameter
 
     def selected(self):
+        #changes status of the button to True(selected) or False(unselected), based on current status and returns that value
         if not self.status:
             self.status = True
             return True
@@ -24,6 +27,7 @@ class Button:
             return False
 
     def update(self, x, y, status):
+        #updates the position of the button and status if needed
         self.x = x
         self.y = y
         self.status = status
--- src/genre.py
+++ src/genre.py
@@ -1,7 +1,8 @@
 class Genre:
+    #This is the list of selected genres#
     def __init__(self):
         self.selected_genres = []
-    
+    #Adding and removing selected genres from the list
     def addRemove(self, genre, status):
         if status:
             self.selected_genres.append(genre)
