c6269f496de3f48cd5bdf449a40d29b0839d3f3a
Daniel Zheng <dzheng47@binghamton.edu>
2021-11-12 13:39:53 -0500

    tested the genre list

--- src/genre.py
+++ src/genre.py
@@ -1,5 +1,8 @@
 class Genre:
     #This is the list of selected genres#
+    #I made the genre list feature to be separate from the button class so that genre buttons wont each have a list on their own#
+    #we would use this as a way to store selected genres and it would involve taking values from genre buttons
+
     def __init__(self):
         self.selected_genres = []
     #Adding and removing selected genres from the list
--- test.py
+++ test.py
@@ -1,11 +1,44 @@
 import pygame
-from pygame import display
+import sys
 from src import button
+from src import genre
+
 t=True
+
+# test to see if I can change the values
 pygame.init()
-pygame.display.set_mode((100,100))
-bob = button.Button("back", 3, 4, "assets/class_diagram.jpg")
-print(bob.type)
-bob.type = "front"
-print(bob.type)
-print(bob.x, bob.y)
+pygame.display.set_mode((600,600))
+testButton = button.Button(3, 4, "assets/class_diagram.jpg")
+print(testButton.x, testButton.y)
+# test to see if I can change the values
+
+#test to see if adding and removing genres to the list(genre class) works
+
+testButton.genreButton("Action")    #gives the test button a genre       
+testGenre = genre.Genre()   #creates a genre list
+
+testButton.selected()   #makes the status of test button as selected
+testGenre.addRemove(testButton.genre, testButton.status)    #adds genre to list
+print(testGenre.selected_genres)    #print out list
+
+testButton.selected()   #makes the status of test button unselected
+testGenre.addRemove(testButton.genre, testButton.status)    #removes genre from list
+print(testGenre.selected_genres)    #prints out list
+
+# test to see if the list works
+for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+#test to see if continous list works
+while True:
+    state =input("selected?")
+    if state == 'true' or state == 'True':
+        #there is a problem with having an else here because 
+        #that would continously remove the genre if false which results in an error
+        #so we want base it only if it is true that the button was clicked on
+        #the condition above is just placeholder
+        testButton.selected()
+        testGenre.addRemove(testButton.genre, testButton.status)
+        print(testGenre.selected_genres)
+#test to see if continous list works
+
