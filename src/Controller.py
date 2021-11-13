9ff93d7ce06a70335f23b8de107399239598d323
Argozan <whuang88@binghamton.edu>
2021-11-12 22:20:15 -0500

    changed controller

--- src/Controller.py
+++ src/Controller.py
@@ -1,5 +1,6 @@
-from src import Button
 import pygame
+
+from src import Button
 
 class Controller:
     def __init__(self):
@@ -10,19 +11,28 @@ class Controller:
         pygame.init()
         self.screen = pygame.display.set_mode((self.window_height, self.window_width))
         self.background = pygame.Surface((self.window_height, self.window_width))
-        self.background.fill(173, 216, 230)
-        self.select_button = Button.Button(x, y, "assets/selectbutton.png")
+        self.background.fill((173, 216, 230))
+        self.select_button = Button.Button(100, 100, "assets/selectbutton.png", 1)
 
-    def mainloop(self):
+    def mainLoop(self):
         while self.state:
             if self.state == "Main screen":
-                self.gameloop()
+                self.gameLoop()
             elif self.state == "Second screen":
-                self.gameloop()
+                self.gameLoop()
             elif self.state == "Third screen":
-                self.gameloop()
+                self.gameLoop()
 
-    def gameloop(self):
+    def gameLoop(self):
         while self.state == "Main screen":
             self.gameEventLoop()
-            self.Button.update()
+            self.display.update()
+    def gameEventLoop(self):
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self.select_button
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    self.player.move("U")
+
+
