45a298b146566c050f15cfc567b9ea422aba9ec1
Daniel Zheng <dzheng47@binghamton.edu>
2021-11-13 21:18:45 -0500

    added in api request for genres to List.py

--- src/List.py
+++ src/List.py
@@ -1,3 +1,6 @@
+import json
+import requests
+
 class Genre:
     #This is the list of selected genres#
     #I made the genre list feature to be separate from the button class so that genre buttons wont each have a list on their own#
@@ -5,7 +8,18 @@ class Genre:
 
     def __init__(self):
         self.selected_genres = []
+        self.all_genres = self.genreList()
     #Adding and removing selected genres from the list
+
+    def genreList(self):
+        genreRequest = requests.get("https://api.themoviedb.org/3/genre/movie/list?api_key=ae2a71b3aac0b67e745c46b2ff92ecb9&language=en-US")
+        conversion = json.loads(genreRequest.text)
+        genreDicts = conversion['genres']
+        allGenres = []
+        for genre in genreDicts:
+            allGenres.append(genre['name'])
+        return allGenres
+
     def addRemove(self, genre, status):
         if status:
             self.selected_genres.append(genre)
