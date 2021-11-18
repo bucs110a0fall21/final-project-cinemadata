6fe2081e45e33d897b6d2b7f5270d1308f38fed1
Daniel Zheng <dzheng47@binghamton.edu>
2021-11-17 21:25:42 -0500

    tested moving buttons in test.py

--- test.py
+++ test.py
@@ -47,3 +47,109 @@ from src import List
 # ###Tests part 2(api requests)###
 # y = List.Genre()
 # print(y.all_genres)
+# Setting up pygame/window --------------------------------
+import pygame
+import sys
+import random
+from pygame.locals import *
+from src import Button
+
+
+class Controller:
+    def __init__(self):
+        # ---------------------------------
+        self.running, self.playing = True, False
+        self.mouse_click = False
+# creating the background ---------------------------------
+        self.window_height = 720
+        self.window_width = 1280
+        self.state = "Main screen"
+        pygame.init()
+        pygame.display.set_caption("SuggestCinema")  # Title of program
+        self.screen = pygame.display.set_mode((self.window_width, self.window_height))
+        self.background = pygame.Surface((self.window_width, self.window_height))
+        self.font_name = pygame.font.get_default_font()
+        self.screen.fill((130, 210, 220)) #RGB colors
+
+        logopng = pygame.image.load('assets/screenlogo.png')  # loading logo on screen
+        self.screen.blit(logopng, (377, 0))
+
+        databasepng = pygame.image.load('assets/moviedb.png')
+        self.screen.blit(databasepng, (1000, 0))
+
+        self.buttonicon = Button.Button(500, 300, "assets/buttonicon.png", 1, 'Genre')
+        self.screen.blit(self.buttonicon.image, (500, 300))
+        self.buttonicon = Button.Button(500, 350, "assets/buttonicon.png", 1, 'Genre')
+        self.screen.blit(self.buttonicon.image, (500, 350))
+        position = pygame.mouse.get_pos()
+        print(position)
+
+        # Attempting to add/create 3 buttons
+        # self.buttons = pygame.sprite.Group()
+        # buttons_num = 3
+        #
+        # for i in range(buttons_num):
+        #     x = random.randrange(100, 400)
+        #     y = random.randrange(100, 400)
+        #     self.buttons.add(Button.Button("Genre", x, y, 'assets/selectbutton'))
+            # self.all_sprites = pygame.sprite.Group(self.buttons)
+
+    def mainLoop(self):
+        while self.state:
+            if self.state == "Main screen":
+                self.gameLoop()
+            elif self.state == "Second screen":
+                self.gameLoop()
+            elif self.state == "Third screen":
+                self.gameLoop()
+
+    def gameLoop(self):
+        # while self.playing:
+        #     self.check_events()
+        #     if self.start_key:
+        #         self.playing = True
+            # self.display.fill(self.BLACK) #resets the canvas
+            # self.window.blit(self.display, (0,0))
+            # pygame.display.update()
+            # self.resetKeys()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if(event.key == pygame.K_UP):
+                        self.buttonicon.update(100,200,False)
+
+
+            # update the screen
+            pygame.display.get_surface()
+            self.screen.fill([200,200,200])
+            self.screen.blit(self.buttonicon.image, (self.buttonicon.rect.x, self.buttonicon.rect.y))
+            pygame.display.update()
+
+    def check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: # if player clicks x close window
+                self.running, self.playing = False, False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.key == pygame.MOUSEBUTTONDOWN:
+                    self.start_key = True
+                # self.select_button
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    self.player.move("U")
+    def resetKeys(self):
+        self.start_key = False
+
+def main():
+	pygame.init()
+	team = {"lead": "Kevin Wu", "backend": "Daniel Zheng", "frontend": "Wilson Huang"}
+	print("Software Lead is:", team["lead"])
+	print("Backend is:", team["backend"])
+	print("Frontend is:", team["frontend"])
+
+	main_window = Controller()
+	main_window.mainLoop()
+	# while controller.running:    # while the program is running(self.playing is true) it will run gameLoop
+	# 	controller = Controller()
+	# 	controller.gameLoop()
+main()
