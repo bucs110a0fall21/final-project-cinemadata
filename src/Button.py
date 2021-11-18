608ba71d6f30462f41ad5d35bf55e10e17051d81
KevinWu262 <kevinwu163@gmail.com>
2021-11-18 00:13:56 -0500

    Overhauled controller, added new asset, replaced screenlogo.png with transparent background. First screen WIP, halfway done

--- src/Button.py
+++ src/Button.py
@@ -2,37 +2,37 @@ import pygame
 
 class Button(pygame.sprite.Sprite):
     def __init__(self, x, y, img_file, scale, text):
-        #initiates the button values
-
-        pygame.sprite.Sprite.__init__(self)
+        super().__init__()
+        #creating rectangle and scaling image
         self.image = pygame.image.load(img_file).convert_alpha()
         self.scale = scale
         self.width = self.image.get_width()
         self.height = self.image.get_height()
-        self.scaled = pygame.transform.scale(self.image, (int(self.width * self.scale), int(self.height * self.scale)))
-        #self.label
+        self.image = pygame.transform.scale(self.image, (int(self.width * self.scale), int(self.height * self.scale)))
         self.rect = self.image.get_rect()
+        #creating label
         self.rect.x = x
         self.rect.y = y
-        self.status = False
         self.label = text
         self.text = pygame.font.SysFont(None, 30)
         self.message = self.text.render(text, True, (255, 255, 255))
-        self.message_rect = self.message.get_rect(center = (self.width / 2, self.height/2))
+        self.message_rect = self.message.get_rect(center=(self.width / 2, self.height/2))
         self.image.blit(self.message, self.message_rect)
+        #button state
+        self.status = False
 
 
-    def selected(self):
-        #changes status of the button to True(selected) or False(unselected), based on current status and returns that value
-        if not self.status:
-            self.status = True
-            return True
-        else:
-            self.status = False
-            return False
-
-    def update(self, x, y, status):
-        #updates the position of the button and status if needed
-        self.rect.x = x
-        self.rect.y = y
-        self.status = status
+    # def selected(self):
+    #     #changes status of the button to True(selected) or False(unselected), based on current status and returns that value
+    #     if not self.status:
+    #         self.status = True
+    #         return True
+    #     else:
+    #         self.status = False
+    #         return False
+    #
+    # def update(self, x, y, status):
+    #     #updates the position of the button and status if needed
+    #     self.rect.x = x
+    #     self.rect.y = y
+    #     self.status = status
--- src/Controller.py
+++ src/Controller.py
@@ -1,111 +1,69 @@
-# Setting up pygame/window --------------------------------
 import pygame
 import sys
-import random
-from pygame.locals import *
 from src import Button
-
+from src import List
 
 class Controller:
     def __init__(self):
-        # ---------------------------------
-        self.running, self.playing = True, False
-        self.mouse_click = False
-# creating the background ---------------------------------
-        self.window_height = 720
-        self.window_width = 1280
-        self.state = "Main screen"
-        pygame.init()
+        super().__init__()
+        self.width = 1080
+        self.height = 720
+        self.state = "MAIN"
         pygame.display.set_caption("SuggestCinema")  # Title of program
-        self.screen = pygame.display.set_mode((self.window_width, self.window_height))
-        self.background = pygame.Surface((self.window_width, self.window_height))
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.background = pygame.Surface((self.width, self.height))
         self.font_name = pygame.font.get_default_font()
-        self.screen.fill((130, 210, 220)) #RGB colors
+        self.screen.fill((130, 210, 220)) #background color
+        self.genre_list = pygame.sprite.Group()
+        self.clicked = False
 
-        logopng = pygame.image.load('assets/screenlogo.png')  # loading logo on screen
-        self.screen.blit(logopng, (377, 0))
-
-        databasepng = pygame.image.load('assets/moviedb.png')
-        self.screen.blit(databasepng, (1000, 0))
-
-        self.searchbutton = Button.Button(100, 109, "assets/searchbutton.png", 1, 'search')
-        searchbutton = pygame.image.load('assets/searchbutton.png')
-        self.screen.blit(searchbutton, (1050, 600))
-
-        buttonicon = Button.Button(100, 109, "assets/buttonicon.png", 1, 'Genre')
-        self.screen.blit(buttonicon.image, (10, 100))
-
-        self.buttonicon = Button.Button(100, 109, "assets/buttonicon.png", 1, 'Genre')
-        self.screen.blit(self.buttonicon.image, (10, 200))
-
-        self.buttonicon = Button.Button(100, 109, "assets/buttonicon.png", 1, 'genre')
-        buttonicon = pygame.image.load('assets/buttonicon.png')
-        self.screen.blit(buttonicon, (10, 300))
-
-        image = pygame.image.load('assets/buttonicon.png')
-        sprite = pygame.sprite.Sprite()
-        sprite.image = image
-        sprite.rect = image.get_rect()
-
-        font = pygame.font.SysFont('Sans', 40)
-        text = font.render('Genre 1', True, (255, 255, 255))
-        # sprite.image.blit(text, sprite.rect)
-        self.screen.blit(text, (50, 150))
-        group = pygame.sprite.Group()
-        group.add(sprite)
-        group.draw(self.screen)
-        pygame.display.flip()
-
-        position = pygame.mouse.get_pos()
-        print(position)
-
-        # Attempting to add/create 3 buttons
-        # self.buttons = pygame.sprite.Group()
-        # buttons_num = 3
-        #
-        # for i in range(buttons_num):
-        #     x = random.randrange(100, 400)
-        #     y = random.randrange(100, 400)
-        #     self.buttons.add(Button.Button("Genre", x, y, 'assets/selectbutton'))
-            # self.all_sprites = pygame.sprite.Group(self.buttons)
+        #first screen
+        genreList = ["Action", "Adventure", "Animation", "Comedy", "Crime", "Documentary"]  # placeholder list, use API to request for the list
+        # setting up buttons
+        x_pos = 467
+        y_pos = 150
+        for genre in genreList:
+            y_pos += 55
+            self.genre_list.add(Button.Button(x_pos, y_pos, "assets/buttonicon.png", 1, genre))
+        self.exit_button = Button.Button(900, 600, "assets/buttonicon.png", 1, "Exit")
+        self.search_button = Button.Button(900, 400, "assets/buttonicon.png", 1, "Search")
+        self.first_screen_sprites = pygame.sprite.Group(tuple(self.genre_list) + (self.exit_button,) + (self.search_button,))
 
     def mainLoop(self):
         while self.state:
-            if self.state == "Main screen":
-                self.gameLoop()
-            elif self.state == "Second screen":
-                self.gameLoop()
-            elif self.state == "Third screen":
-                self.gameLoop()
+            if self.state == "MAIN":
+                self.firstScreenLoop()
+            elif self.state == "SECOND":
+                self.secondScreenLoop()
+            elif self.state == "THIRD":
+                self.thirdScreenLoop()
 
-    def gameLoop(self):
-        # while self.playing:
-        #     self.check_events()
-        #     if self.start_key:
-        #         self.playing = True
-            # self.display.fill(self.BLACK) #resets the canvas
-            # self.window.blit(self.display, (0,0))
-            # pygame.display.update()
-            # self.resetKeys()
+    def firstScreenLoop(self):
+        while self.state == "MAIN":
+            #setting up static images
+            logo = pygame.image.load('assets/screenlogo.png')
+            self.screen.blit(logo, (277, 0))
+            tmdb_logo = pygame.image.load('assets/moviedb.png')
+            self.screen.blit(tmdb_logo, (800, 0))
+            self.first_screen_sprites.draw(self.screen)
+            #loop
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if self.exit_button.rect.collidepoint(event.pos):
+                        sys.exit()
+                    elif self.search_button.rect.collidepoint(event.pos):
+                        self.state = "SECOND"
+                    #WIP
+                    for button in self.genre_list:
+                        if button.rect.collidepoint(event.pos):
+                            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                                self.clicked = True
+                                print("Clicked")
+                        if pygame.mouse.get_pressed()[0] == 0:
+                            self.clicked = False
+
+
             pygame.display.flip()
-            self.check_events()
 
-            pygame.display.get_surface()
-            pygame.display.update()
-
-    def check_events(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: # if player clicks x close window
-                self.running, self.playing = False, False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.key == pygame.MOUSEBUTTONDOWN:
-                    self.start_key = True
-                # self.select_button
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    self.player.move("U")
-    def resetKeys(self):
-        self.start_key = False
