2a1c295e73592ea12fe6a0b4cba909ac1fdd9e2c
KevinWu262 <kevinwu163@gmail.com>
2021-11-29 19:36:44 -0500

    Essentials of main screen GUI complete

--- src/Controller.py
+++ src/Controller.py
@@ -4,15 +4,6 @@ from src import Button
 from src import APIrequest
 
 class Controller:
-    """
-    Creates screen and sets up button class
-    Args:
-        width (int) - x pos of screen
-        height (int) - y pos of screen
-        state (str) - state of the game
-
-    return: none
-    """
     def __init__(self):
         super().__init__()
         self.width = 1080
@@ -38,12 +29,13 @@ class Controller:
             self.genre_list.add(Button.Button(x_pos, y_pos, "assets/buttonicon.png", 1, genre['name'], genre['name'], genre['id']))
         self.exit_button = Button.Button(900, 600, "assets/buttonicon.png", 1, "Exit")
         self.search_button = Button.Button(900, 400, "assets/buttonicon.png", 1, "Search")
+        self.back_button = Button.Button(900, 500, "assets/buttonicon.png", 1, "Back")
         self.logo = pygame.image.load('assets/screenlogo.png')
         self.tmdb_logo = pygame.image.load('assets/moviedb.png')
-        self.first_screen_sprites = pygame.sprite.Group(tuple(self.genre_list) + tuple(self.user_genre_buttons) + (self.exit_button,) + (self.search_button,))
+        self.first_screen_sprites = pygame.sprite.Group(tuple(self.genre_list) + (self.exit_button,) + (self.search_button,))
 
         #second screen
-        self.second_screen_sprites = pygame.sprite.Group((self.exit_button,))
+        self.second_screen_sprites = pygame.sprite.Group((self.exit_button,) + (self.back_button,))
 
     def mainLoop(self):
         while self.state:
@@ -55,7 +47,7 @@ class Controller:
                 self.thirdScreenLoop()
 
     def firstScreenLoop(self):
-        x_pos = 0
+        x_pos = 15
         y_pos = 200
         while self.state == "MAIN":
             y_offset = 0
@@ -71,31 +63,39 @@ class Controller:
                         y_offset -= 15
                     #exit button
                     if self.exit_button.rect.collidepoint(event.pos):
-                        sys.exit()
+                        if pygame.mouse.get_pressed()[0] == 1:
+                            sys.exit()
                     #search button
                     elif self.search_button.rect.collidepoint(event.pos):
-                        temp = APIrequest.APIrequest(self.user_selected_ids)
-                        print(temp.apiRequest())
-                        self.state = "SECOND"
+                        if pygame.mouse.get_pressed()[0] == 1:
+                            self.state = "SECOND"
                     #center genre buttons
                     for button in self.genre_list:
-                        if button.rect.collidepoint(event.pos):
-                            if pygame.mouse.get_pressed()[0] == 1:
-                                if button.label in self.user_genre_list:
-                                    pass
-                                else:
-                                    self.user_genre_list.append(button.label)
-                                    self.user_selected_ids.append(button.id)
-                                    #left side buttons - WIP
-                                    y_pos += 55
-                                    self.user_genre_buttons.add(
-                                        Button.Button(x_pos, y_pos, "assets/buttonicon.png", 1, button.label, button.label, button.id)
-                                    )
-                                #printing for testing purposes
-                                print(button.label)
-                                print(button.id)
+                        if button.rect.collidepoint(event.pos) and pygame.mouse.get_pressed()[0] == 1:
+                            if button.label in self.user_genre_list:
+                                pass
+                            elif button not in self.user_genre_buttons:
+                                button_var = Button.Button(15, y_pos, 'assets/buttonicon.png', 1, button.label, button.label, button.id)
+                                self.user_genre_buttons.add(button_var)
+                                self.user_genre_list.append(button.label)
+                                self.user_selected_ids.append(button.id)
+                                # printing for testing purposes
                                 print(self.user_genre_list)
                                 print(self.user_selected_ids)
+                    #left side buttons
+                    y_pos = 200
+                    for button in self.user_genre_buttons:
+                        if button.rect.collidepoint(event.pos) and pygame.mouse.get_pressed()[0] == 1:
+                            button.kill()
+                            self.user_genre_list.remove(button.label)
+                            self.user_selected_ids.remove(button.id)
+                            # printing for testing purposes
+                            print(self.user_genre_list)
+                            print(self.user_selected_ids)
+                        else:
+                            button.rect.y = y_pos
+                            y_pos += 55
+
 
             #update
             self.screen.fill((130, 210, 220))
@@ -104,11 +104,14 @@ class Controller:
             for button in self.genre_list:
                 button.update(y_offset)
             self.first_screen_sprites.draw(self.screen)
+            self.user_genre_buttons.draw(self.screen)
 
             #draw
             pygame.display.update()
 
     def secondScreenLoop(self):
+        movie_data = APIrequest.APIrequest(self.user_selected_ids)
+        print(movie_data.apiRequest())
         while self.state == "SECOND":
             #check for events
             for event in pygame.event.get():
@@ -116,7 +119,11 @@ class Controller:
                     sys.exit()
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if self.exit_button.rect.collidepoint(event.pos):
-                        sys.exit()
+                        if pygame.mouse.get_pressed()[0] == 1:
+                            sys.exit()
+                    if self.back_button.rect.collidepoint(event.pos):
+                        if pygame.mouse.get_pressed()[0] == 1:
+                            self.state = "MAIN"
 
 
             #update
